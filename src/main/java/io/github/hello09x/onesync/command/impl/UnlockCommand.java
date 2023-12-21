344f6d29fb65198249f5144fd5ce1feb2cb82689
tanyaofei <tanyaofei@outlook.com>
2023-12-21 18:09:45 +0800

    优化代码

--- src/main/java/io/github/hello09x/onesync/command/impl/UnlockCommand.java
+++ src/main/java/io/github/hello09x/onesync/command/impl/UnlockCommand.java
@@ -2,7 +2,7 @@ package io.github.hello09x.onesync.command.impl;
 
 import dev.jorel.commandapi.executors.CommandArguments;
 import io.github.hello09x.onesync.manager.LockingManager;
-import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -11,9 +11,10 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.Objects;
 
-import static net.kyori.adventure.text.Component.*;
-import static net.kyori.adventure.text.format.NamedTextColor.*;
-import static net.kyori.adventure.text.format.TextDecoration.BOLD;
+import static net.kyori.adventure.text.Component.empty;
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
+import static net.kyori.adventure.text.format.NamedTextColor.WHITE;
 
 public class UnlockCommand {
 
@@ -22,6 +23,19 @@ public class UnlockCommand {
 
     private final static MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
+    private final static List<Component> MESSAGES = List.of(
+            text("=========================================================================", GRAY),
+            MINI_MESSAGE.deserialize("<b>「锁」</b>是用在玩家登陆时确保上一个服务器的数据写入完毕"),
+            MINI_MESSAGE.deserialize("<gray> <green>+</green> 玩家<white>加入</white>服务器恢复数据后, 对玩家<white>上锁</white></gray>"),
+            MINI_MESSAGE.deserialize("<gray> <green>+</green> 玩家<white>退出</white>服务器保存数据后, 对玩家<white>解锁</white></gray>"),
+            empty(),
+            MINI_MESSAGE.deserialize("<gray><i>当服务器异常关闭, 数据库异常, 没有完成解锁的过程时才需要执行此命令</i></gray>"),
+            MINI_MESSAGE.deserialize("<gray>此命令作用后, 将会释放所有锁</gray>, <gold>并让所有服务器重新对当前在线的玩家上锁</gold>"),
+            text("=========================================================================", GRAY),
+            text("解锁成功", WHITE),
+            text("=========================================================================", GRAY)
+    );
+
     public void unlock(@NotNull CommandSender sender, @NotNull CommandArguments args) {
         var player = (OfflinePlayer) Objects.requireNonNull(args.get("player"));
         if (player.getPlayer() != null) {
@@ -29,25 +43,12 @@ public class UnlockCommand {
             return;
         }
         manager.removeLock(player);
-        sender.sendMessage(text("解锁成功", GRAY));
+        MESSAGES.forEach(sender::sendMessage);
     }
 
     public void unlockAll(@NotNull CommandSender sender, @NotNull CommandArguments args) {
         manager.removeAllLocks();
-
-        var messages = List.of(
-                text("=========================================================================", GRAY),
-                MINI_MESSAGE.deserialize("<b>「锁」</b>是用在玩家登陆时确保上一个服务器的数据写入完毕"),
-                MINI_MESSAGE.deserialize("<gray> <green>+</green> 玩家<white>加入</white>服务器恢复数据后, 对玩家<white>上锁</white></gray>"),
-                MINI_MESSAGE.deserialize("<gray> <green>+</green> 玩家<white>退出</white>服务器保存数据后, 对玩家<white>解锁</white></gray>"),
-                empty(),
-                MINI_MESSAGE.deserialize("<gray><i>当服务器异常关闭, 数据库异常, 没有完成解锁的过程时才需要执行此命令</i></gray>"),
-                MINI_MESSAGE.deserialize("<gray>此命令作用后, 将会释放所有锁</gray>, <gold>并让所有服务器重新对当前在线的玩家上锁</gold>"),
-                text("=========================================================================", GRAY),
-                text("解锁成功", WHITE),
-                text("=========================================================================", GRAY)
-        );
-        messages.forEach(sender::sendMessage);
+        MESSAGES.forEach(sender::sendMessage);
     }
 
 }
